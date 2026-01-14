c328ff930d87b3acf68395f5fa7dfa759d793d67
InfiniteC0re <inf.core0@gmail.com>
2026-01-15 04:05:07 +0500

    ModCore: fix \0 byte missing

--- SDK/JPOGModCore/Source/AHooks.cpp
+++ SDK/JPOGModCore/Source/AHooks.cpp
@@ -112,6 +112,7 @@ MEMBER_HOOK(Interface::TRenderInterface + 0x9730, Toshi::TModel, TModel_LoadTMD,
 		if (pchNameStart && *pchNameStart && pchExtStart && *pchExtStart)
 		{
 			strncpy(s_ModelBuffer, pchNameStart, pchExtStart - pchNameStart);
+			s_ModelBuffer[pchExtStart - pchNameStart] = '\0';
 
 			static TCHAR s_PathBuffer[MAX_PATH];
 			sprintf_s(s_PathBuffer, "Data\\matlibs\\%s.tml", s_ModelBuffer);
